0fd6b9a08ab379ac3f97a2ecf1876c8e8de3d963
Abdo <abdo@axetay.co.uk>
2020-05-15 15:40:59 +0100

    update action

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,9 @@ import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
+    const whoToGreet: string = core.getInput('who-to-greet')
+    core.debug(`hello ${whoToGreet}`)
+    
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
     core.setFailed(error.message)
