aff0f4f9e7d81d73cddcb14b19969f7886dda742
Abdo <abdo@axetay.co.uk>
2020-05-15 16:12:58 +0100

    fix tests

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
     const whoToGreet: string = core.getInput('who-to-greet')
     core.debug(`hello ${whoToGreet}`)
-    
+
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
     core.setFailed(error.message)
